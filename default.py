74167e6de40288766fb430273577e1644580ad17
Cyril <cyril.leclerc@gmail.com>
2012-01-05 02:36:52 +0100

    remove int cast on id (api looks for leading 0)

--- default.py
+++ default.py
@@ -1110,8 +1110,8 @@ grooveshark = Grooveshark();
 qob._handle = grooveshark._handle
 
 
-id=0
-try: id=int(params["id"])
+id=''
+try: id=params["id"]
 except: pass
 name = None
 try: name=urllib.unquote_plus(params["name"])
@@ -1181,7 +1181,7 @@ elif mode == MODE_SONG:
 #     
 elif mode==MODE_ALBUM:
      print "Product ID: " + str(id) + "\n"
-     grooveshark.product(id)
+     grooveshark.product(str(id))
      
      
 elif mode==MODE_PLAYLIST:
