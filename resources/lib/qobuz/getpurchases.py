9c30a2771519b1a4da05266dff64c32a0f027ac3
Cyril Leclerc <cyril.leclerc@gmail.com>
2012-01-12 00:36:24 +0100

    try album view in purchases

--- resources/lib/qobuz/getpurchases.py
+++ resources/lib/qobuz/getpurchases.py
@@ -51,13 +51,48 @@ class QobuzGetPurchases(ICacheable):
 
     def add_to_directory(self):
         n = self.length()
-        xp = self.Core.Bootstrap.Player.Playlist
-        xp.clear()
-        i = 0
+
+        #i = 0
+        albumseen = {}
+
         for track in self._raw_data:    
             t = QobuzTagTrack(self.Core, track)
-            item = t.getXbmcItem('songs')
-            u = sys.argv[0] + "?mode=" + str(MODE_SONG) + "&id=" + t.id + "&pos=" + str(i) + "&context_type=purchases" 
-            self.Core.Bootstrap.GUI.addDirectoryItem(u , item, False, n)
-            xp.add(u, item)
-            i = i + 1
+            pprint.pprint(t)
+            albumid = t.getAlbumId()
+            log ('warn',albumid)
+            isseen = 'false'
+            try:
+                 isseen = albumseen[albumid]
+            except: pass
+            if isseen == 'false':
+                   #if  albumseen[albumid] is false:
+                log ('warn','album never seen try to add it')
+                try:
+                    album = self.Core.getProduct(albumid)
+                    a = QobuzTagProduct(self.Core, album)
+                    item = a.getXbmcItem()
+                    albumid = a.id     
+                    u = sys.argv[0] + "?mode=" + str(MODE_ALBUM) + "&id=" + str(albumid)
+                    item.setPath(u)
+                    item.setProperty('path', u)
+                    xbmcplugin.addDirectoryItem(handle=self.Core.Bootstrap.__handle__, 
+                                                url=u, listitem=item, isFolder=True, 
+                                                totalItems=self.length())                     
+                except:                             
+                    #log ('warn', "album not found...")
+                    albumseen[albumid] = 'true'
+                      
+           #except:pass
+           
+            
+           #item = t.getXbmcItem('songs')
+           #u = sys.argv[0] + "?mode=" + str(MODE_SONG) + "&id=" + t.id + "&pos=" + str(i) + "&context_type=purchases" 
+           #self.Core.Bootstrap.GUI.addDirectoryItem(u , item, False, n)
+           #xp.add(u, item)
+           #i = i + 1
+        
+        
+        #for product in self.get_data():
+            
+
+        return self.length()
