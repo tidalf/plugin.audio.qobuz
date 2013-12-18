1929b8d42696b5542c67dc34b828b1a844bafe3c
sho <sho@wi.net>
2013-12-18 21:14:37 +0100

    :xbmc-qobuz: setup.py added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xbmc-qobuz",
+    version="1.0.2",
+    packages=find_packages(),
+    scripts=[],
+    install_requires=['requests'],
+    dependency_links=[],
+    author="d@corp",
+    author_email="",
+    description="",
+    license="GPL",
+    keywords="",
+    url="",
+)
