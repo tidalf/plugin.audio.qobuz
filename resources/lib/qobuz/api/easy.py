19926d139f0daea4618b38162ceaf69fc7bdd686
sho <sho@wi.net>
2013-12-18 21:08:19 +0100

    :qobuz.api.easy: PEP8

--- resources/lib/qobuz/api/easy.py
+++ resources/lib/qobuz/api/easy.py
@@ -2,14 +2,15 @@
     qobuz.api.easy
     ~~~~~~~~~~~~~~~~~~
 
-    Add 'get' to qobuz.api.raw, All requests made trough this method are 
+    Add 'get' to qobuz.api.raw, All requests made trough this method are
     cached (see qobuz.cache.qobuz)
-    
+
     :copyright: (c) 2012 by Joachim Basmaison, Cyril Leclerc
     :license: GPLv3, see LICENSE for more details.
 '''
 from cache import cache
 from api.raw  import QobuzApiRaw
+
 
 class InvalidQuery(Exception):
     pass
@@ -27,7 +28,7 @@ class QobuzApiEasy(QobuzApiRaw):
     @cache.cached
     def get(self, *a, **ka):
         '''Wrapper that cache query to our raw api. We are enforcing format
-        because cache entry key are made based on *a and **ka parameters. 
+        because cache entry key are made based on *a and **ka parameters.
         ('artist/get' and '/artist/get' will generate different key)
         Path are mapped to raw api and raise InvalidQuery on error
 
@@ -36,22 +37,22 @@ class QobuzApiEasy(QobuzApiRaw):
         from cache import cache
         cace.base_path = '/srv/qobuz/cache/'
         data = api.get('/artist/get')
-        data = api.get('/user/login', 
-                        username=api.username, 
+        data = api.get('/user/login',
+                        username=api.username,
                         password=api.password)
-                        
+
         :: note Named parameters are sorted before we generate our key
-        
+
         ::return
             Pyton Dictionary on success
             None on error
-            
+
         ::note api.error will contain last error message
         '''
         if not a[0] or not a[0].startswith('/'):
             raise InvalidQuery("Missing starting << / >>")
         path = '/'.join(a)
-        path.replace('//', '/') # Defected for n / ...
+        path.replace('//', '/')  # Defected for n / ...
         path = path[1:]
         if path.endswith('/'):
             raise InvalidQuery('Invalid trailing << / >>')
@@ -68,7 +69,7 @@ class QobuzApiEasy(QobuzApiRaw):
         return getattr(self, methname)(**ka)
 
     def __clean_ka(self, endpoint, method, **ka):
-        ''' We are removing some key that are not needed by our raw api but 
+        ''' We are removing some key that are not needed by our raw api but
         generate different cache entry (Data bound to specific user...) '''
         keys = []
         if endpoint == 'track' and method == 'getFileUrl':
@@ -78,12 +79,12 @@ class QobuzApiEasy(QobuzApiRaw):
             if 'user_id' in ka:
                 keys.append('user_id')
         return keys
-        
+
     def login(self, username, password):
-        '''We are storing our authentication token back to our raw api on 
+        '''We are storing our authentication token back to our raw api on
         success.
 
-        ::return 
+        ::return
             True on success, else False
         '''
         self.username = username
