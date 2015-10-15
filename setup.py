207dafc9ce36dd658c07ad952bce4507bc6f1c86
showi <joachim.basmaison@gmail.com>
2015-10-15 04:41:43 +0200

    Update to version 1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xbmc-qobuz",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     scripts=[],
     install_requires=['requests'],
