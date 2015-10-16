ce30ecee0105d74f6c9a3b5547f5deb254cd7e62
showi <joachim.basmaison@gmail.com>
2015-10-16 13:59:31 +0200

    Version 1.2.1 XBMC string ref. to Kodi, plugin renamed: Qobuz Changelog update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xbmc-qobuz",
-    version="1.0.3",
+    version="1.2.1",
     packages=find_packages(),
     scripts=[],
     install_requires=['requests'],
