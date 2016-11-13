0076c8c99a4195b7b4252f1f0245073ef1219450
sho <joachim.basmaison@gmail.com>
2016-11-13 15:55:50 +0100

    add copyleft

---
+++ resources/lib/qobuz/context/__init__.py
@@ -0,0 +1,8 @@
+'''
+    qobuz.context
+    ~~~~~~~~~~~~~
+
+    :part_of: xbmc-qobuz
+    :copyright: (c) 2012-2016 by Joachim Basmaison, Cyril Leclerc
+    :license: GPLv3, see LICENSE for more details.
+'''
--- resources/lib/qobuz/context/similar_artist.py
+++ resources/lib/qobuz/context/similar_artist.py
@@ -1,3 +1,11 @@
+'''
+    qobuz.context.similar_artist
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :part_of: xbmc-qobuz
+    :copyright: (c) 2012-2016 by Joachim Basmaison, Cyril Leclerc
+    :license: GPLv3, see LICENSE for more details.
+'''
 import sys
 from os import path as P
 base_dir = P.abspath(P.join(P.dirname(__file__), P.pardir))
