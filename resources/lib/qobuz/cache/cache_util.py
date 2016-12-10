e1d096d146ccb677af6c8099b4ed411959124638
sho <joachim.basmaison@gmail.com>
2016-12-10 02:54:09 +0100

    Useless comment

--- resources/lib/qobuz/cache/cache_util.py
+++ resources/lib/qobuz/cache/cache_util.py
@@ -12,9 +12,8 @@ import os
 from qobuz.util.file import find
 from qobuz import debug
 
+
 def clean_old(cache):
-    """Callback deleting one file
-    """
     def delete_one(filename, info):
         data = cache.load_from_store(filename)
         if not cache.check_magic(data):
@@ -24,25 +23,25 @@ def clean_old(cache):
             return True
         cache.delete(data['key'])
         return True
+
     find(cache.base_path, '^.*\.dat$', delete_one)
     return True
 
 
 def clean_all(cache):
-    """Clean all data from cache
-    """
     def delete_one(filename, info, check_magic=True):
-        '''::callback that delete one file
-        '''
         data = cache.load_from_store(filename)
         if check_magic and not cache.check_magic(data):
-            debug.error(__name__, "Error: bad magic, skipping file {}", filename)
+            debug.error(__name__, "Error: bad magic, skipping file {}",
+                        filename)
             return True
         cache.delete(data['key'])
         return True
+
     def delete_nocheck(filename, info):
         os.unlink(filename)
         return True
+
     find(cache.base_path, '^.*\.dat$', delete_one)
     find(cache.base_path, '^.*\.local$', delete_nocheck)
     return True
