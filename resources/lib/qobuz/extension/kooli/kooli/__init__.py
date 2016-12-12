25ee19f9afb2870a466c62f6f992e52e293d78ee
sho <joachim.basmaison@gmail.com>
2016-12-12 09:47:53 +0100

    useless loggin

--- resources/lib/qobuz/extension/kooli/kooli/__init__.py
+++ resources/lib/qobuz/extension/kooli/kooli/__init__.py
@@ -11,7 +11,7 @@ import sys
 import os
 from os import path as P
 import logging
-kooli_path = P.abspath(P.join(P.dirname(__file__)))
+kooli_path = P.abspath(P.join(P.dirname≤(__file__)))
 base_path = P.join(kooli_path, P.pardir)
 qobuz_lib_path = P.abspath(P.join(base_path, P.pardir, P.pardir, P.pardir))
 try:
@@ -23,5 +23,3 @@ if not P.exists(log_dir):
     os.makedirs(log_dir)
 log_file = P.join(log_dir, 'extension-kooli.log')
 logging.basicConfig(filename=log_file)
-log = logging.getLogger('kooli')
-log.setLevel(logging.DEBUG)
