a23e919a6f18689caf4232f7963906e59c1af3d4
joachim basmaison <jbasmaison@dashlane.com>
2018-03-08 03:23:53 +0100

    More tests

--- resources/lib/tests/main_test.py
+++ resources/lib/tests/main_test.py
@@ -5,6 +5,10 @@ import unittest
 qobuzPath = P.abspath(P.join(P.dirname(__file__), P.pardir))
 sys.path.append(qobuzPath)
 
+registry_data = {
+}
+
+
 class TestMain(unittest.TestCase):
 
     @classmethod
@@ -20,3 +24,28 @@ class TestMain(unittest.TestCase):
         self.assertIsNotNone(qobuz.base_path)
         self.assertIsNotNone(qobuz.data_path)
         return self
+
+    def test_module_constant(self):
+        from qobuz.constants import Mode
+        self.assertEqual(Mode.VIEW, 0x1)
+        self.assertEqual(Mode.PLAY, 0x2)
+        self.assertEqual(Mode.SCAN, 0x3)
+        self.assertEqual(Mode.VIEW_BIG_DIR, 0x4)
+        self.assertEqual(Mode.to_s(Mode.VIEW), 'view')
+        self.assertEqual(Mode.to_s(Mode.PLAY), 'play')
+        self.assertEqual(Mode.to_s(Mode.SCAN), 'scan')
+        self.assertEqual(Mode.to_s(Mode.VIEW_BIG_DIR), 'view_big_dir')
+
+    def test_module_debug(self):
+        from qobuz.debug import getLogger
+        logger = getLogger(__name__)
+        self.assertIsNotNone(logger)
+
+    def test_module_registry(self):
+        from qobuz.registry import Registry
+        import ConfigParser
+        registry = Registry(None)
+        self.assertIsNotNone(registry)
+        self.assertEqual(registry.get('streamtype'), 'flac')
+        self.assertRaises(ConfigParser.NoOptionError,
+                          registry.get, 'UnknownKey')
